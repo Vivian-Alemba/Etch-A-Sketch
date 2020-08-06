1170ed558fe5f789ec8b82169707f0d1f2ea2182
Vivian-Alemba <alembavivianauma@gmail.com>
2020-08-06 15:22:57 -0700

    file modified

--- script.js
+++ script.js
@@ -5,20 +5,16 @@ function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
 
-  for (c = 0; c < (rows * cols); c++) {
+  for (t = 0; t < (rows * cols); t++) {
     let cell = document.createElement("div")
-         cell.innerText = (c + 1)
+         cell.innerText = (t + 1)
          container.appendChild(cell).className = "grid-item"
 
          cell.addEventListener('mouseover',e => e.target.classList.add('designColor'))
 
 
 
-         let currentColor = 'black'
-
-const colors = { black: 'black' }
-
-e => e.target.classList.add(colors[currentColor])
+         
   }; 
 };
 
